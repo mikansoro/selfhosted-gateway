5ae4ea30505fa490d138460ad8763a3569160923
Michael Rowland <michael@mikansystems.com>
2023-03-01 01:36:00 -0800

    pin random container port to container to survive reboots

--- src/create-link/remote.sh
+++ src/create-link/remote.sh
@@ -11,6 +11,14 @@ CONTAINER_ID=$(docker run --name $CONTAINER_NAME --network gateway -p 18521/udp 
 GATEWAY_LINK_WG_PUBKEY=$(docker exec $CONTAINER_NAME bash -c 'cat /etc/wireguard/link0.key |wg pubkey')
 # get randomly assigned WireGuard port
 WIREGUARD_PORT=$(docker port $CONTAINER_NAME 18521/udp| head -n 1| sed "s/0\.0\.0\.0://")
+
+# hacky pin randomly selected docker port as persistent by editing container hostconfig.json to persist config through reboots
+CONTAINER_CONFIG_PATH=$(docker inspect $CONTAINER_NAME | jq -r '.[]| ."HostsPath"' | xargs dirname)
+docker stop "$CONTAINER_NAME"
+jq --arg PORT "$WIREGUARD_PORT" --arg PORTSEL "${WIREGUARD_PORT}/udp" '(."PortBindings" | .[$PORTSEL] | .[] | ."HostPort" ) |= $PORT' "$CONTAINER_CONFIG_PATH/hostconfig.json" > "$CONTAINER_CONFIG_PATH/hostconfig.json.tmp"
+mv "$CONTAINER_CONFIG_PATH/hostconfig.json.tmp" "$CONTAINER_CONFIG_PATH/hostconfig.json"
+docker start "$CONTAINER_NAME"
+
 # get public ipv4 address
 GATEWAY_IP=$(curl -s 4.icanhazip.com)
 
