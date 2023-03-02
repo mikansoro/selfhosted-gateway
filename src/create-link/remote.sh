6710bfcf05ef46f9d3e318befe492f894efc7a58
Michael Rowland <michael@mikansystems.com>
2023-03-02 01:36:58 -0800

    find a solution that doesn't require restarting dockerd

--- src/create-link/remote.sh
+++ src/create-link/remote.sh
@@ -5,22 +5,17 @@ set -e
 CONTAINER_NAME=$1
 LINK_CLIENT_WG_PUBKEY=$2
 
+# get local port range
+read LOWER_PORT UPPER_PORT < /proc/sys/net/ipv4/ip_local_port_range
+# credit: https://unix.stackexchange.com/a/423052
+# compare active udp ports against local port range, select an open port at random
+WIREGUARD_PORT=$(comm -23 <(seq $LOWER_PORT $UPPER_PORT | sort) <(ss -Huan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -1)
+
 # create gateway-link container
-CONTAINER_ID=$(docker run --name $CONTAINER_NAME --network gateway -p 18521/udp --cap-add NET_ADMIN --restart unless-stopped -it -e LINK_CLIENT_WG_PUBKEY=$LINK_CLIENT_WG_PUBKEY -d fractalnetworks/gateway-link:latest)
+CONTAINER_ID=$(docker run --name $CONTAINER_NAME --network gateway -p $WIREGUARD_PORT:18521/udp --cap-add NET_ADMIN --restart unless-stopped -it -e LINK_CLIENT_WG_PUBKEY=$LINK_CLIENT_WG_PUBKEY -d fractalnetworks/gateway-link:latest)
 # get gateway-link WireGuard pubkey 
 GATEWAY_LINK_WG_PUBKEY=$(docker exec $CONTAINER_NAME bash -c 'cat /etc/wireguard/link0.key |wg pubkey')
-# get randomly assigned WireGuard port
-WIREGUARD_PORT=$(docker port $CONTAINER_NAME 18521/udp| head -n 1| sed "s/0\.0\.0\.0://")
-
-# hacky pin randomly selected docker port as persistent by editing container hostconfig.json to persist config through reboots
-CONTAINER_CONFIG_PATH=$(docker inspect $CONTAINER_NAME | jq -r '.[]| ."HostsPath"' | xargs dirname)
-docker stop "$CONTAINER_NAME" 1>/dev/null 2>&1
-jq --arg PORT "$WIREGUARD_PORT" '(."PortBindings"."18521/udp"[] | ."HostPort" ) |= $PORT' "$CONTAINER_CONFIG_PATH/hostconfig.json" > "$CONTAINER_CONFIG_PATH/hostconfig.json.tmp"
-mv "$CONTAINER_CONFIG_PATH/hostconfig.json.tmp" "$CONTAINER_CONFIG_PATH/hostconfig.json"
-docker start "$CONTAINER_NAME" 1>/dev/null 2>&1
-
 # get public ipv4 address
 GATEWAY_IP=$(curl -s 4.icanhazip.com)
 
 echo "$GATEWAY_LINK_WG_PUBKEY $GATEWAY_IP:$WIREGUARD_PORT"
-
